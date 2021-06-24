acce399576fc386f3c783851405fd86f5f3f7ec5
alcotico <mail-ral@yandex.ru>
2021-06-24 07:24:31 +0500

    v0.5 Добавлено: - графическое сравнение четырех методов интерполяции через subplot'ы.

--- main.py
+++ main.py
@@ -1,14 +1,52 @@
 import interpolation as ipl
 from datagen import datagen as dg
 from numpy.random import seed
+import matplotlib.pyplot as plt
 
 POINTS = 10
 SAMPLING_STEP = 20
 SEED = 666
+TSIZE = 25
+SIZE = 2
 
 if __name__ == '__main__':
     seed(SEED)
-    x, y = dg(POINTS)
-    cls = ipl.Intpl1D(x, y)
-    cls.lagrange(SAMPLING_STEP)
-    cls.show()
+    tx, ty = dg(POINTS)
+    cls = ipl.Intpl1D(tx, ty)
+
+    plt.figure(figsize=(12, 7))
+
+    plt.subplot(2, 2, 1)
+    res = cls.lineal(SAMPLING_STEP)
+    x = [elm[0] for elm in res]
+    y = [elm[1] for elm in res]
+    size = [TSIZE if elm in tx else SIZE for elm in x]
+    plt.scatter(x, y, c='green', s=size, label='Линейная')
+    plt.legend()
+
+    plt.subplot(2, 2, 2)
+    res = cls.square(SAMPLING_STEP)
+    x = [elm[0] for elm in res]
+    y = [elm[1] for elm in res]
+    size = [TSIZE if elm in tx else SIZE for elm in x]
+    plt.scatter(x, y, c='blue', s=size, label='Квадратичная')
+    plt.legend()
+
+    plt.subplot(2, 2, 3)
+    res = cls.cubical(SAMPLING_STEP)
+    x = [elm[0] for elm in res]
+    y = [elm[1] for elm in res]
+    size = [TSIZE if elm in tx else SIZE for elm in x]
+    plt.scatter(x, y, c='orange', s=size, label='Кубическая')
+    plt.legend()
+
+    plt.subplot(2, 2, 4)
+    res = cls.lagrange(SAMPLING_STEP)
+    x = [elm[0] for elm in res]
+    y = [elm[1] for elm in res]
+    size = [TSIZE if elm in tx else SIZE for elm in x]
+    plt.scatter(x, y, c='red', s=size, label='многочлен Лагранжа')
+    plt.legend()
+
+    plt.suptitle('Сравнение методов интерполяции', fontsize=24)
+    plt.show()
